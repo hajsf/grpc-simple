42f493eef743c0a9eaba3e4d3e9ab9e27b035e70
Hasan Yousef <hyousef@msn.com>
2022-02-15 21:23:48 +0300

    simple gRPC server and client

---
+++ server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/hajsf/grpc-simple/chat"
+	"google.golang.org/grpc"
+)
+
+func main() {
+
+	fmt.Println("Go gRPC Beginners Tutorial!")
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 9000))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := chat.Server{}
+
+	// srv := chat.ChatServiceServer{}
+
+	grpcServer := grpc.NewServer()
+
+	chat.RegisterChatServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
+}
